23f8639269e19eb5abd80ce18fd955944330cafe
lazybirth <namgi4@naver.com>
2022-02-26 16:39:29 +0900

    문자열indexing

---
+++ str.py
@@ -0,0 +1,29 @@
+
+
+#print(ch[0])
+#print(ch[1])
+#print(ch[2])
+# [시작:끝:단계]
+
+
+
+
+
+ch = "유달산달리도외달도"
+
+# 유달산 , 달리도, 외달도
+# 유산리외도
+# 도달외도리달산달유
+
+
+ch1 = ch[0:3]
+ch2 = ch[3:6]
+ch3 = ch[6:]
+ch4 = ch[::2]
+ch5 = ch[::-1]
+
+print(ch1)
+print(ch2)
+print(ch3)
+print(ch4)
+print(ch5)
