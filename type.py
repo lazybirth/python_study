bd84c909f7e30651837e60eff9ac8bc3b129bdea
lazybirth <namgi4@naver.com>
2022-02-26 16:25:28 +0900

    타입변환연습

---
+++ type.py
@@ -0,0 +1,25 @@
+#int str float
+
+a = 3
+b = '3'
+c = 3.0000
+
+
+print(type(a), type(b), type(c))
+
+d = int(b)
+
+print(type(d))
+
+
+
+
+ch = '123'
+
+# int 으로 변경
+# float 형태로 변경 해서 출력하는데 type()이거써서 형태를 보이게 출력 ㅇㅋ?
+
+aa = int(ch) # 123
+bb = float(ch) # 123.0
+
+print(type(aa),type(bb))
